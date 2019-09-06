d7553269723f0cd8e038db63f419a7e9bf52731a
danielpatricv <dpvandroid54@gmail.com>
2019-09-06 00:24:36 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegiterComponent } from './login/regiter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+    RegiterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/regiter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regiter',
+  templateUrl: './regiter.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegiterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
